95f34fbc06ffa8ddf48b63660e2212e7b5098e70
smartlove730 <55316874+smartlove730@users.noreply.github.com>
2026-02-10 16:20:03 +0530

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     @yield('SeoTags')
-
+<meta name="google-adsense-account" content="ca-pub-3230339294601454">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="viewport" content="width=device-width, initial-scale=1">
