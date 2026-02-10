3bda355828510b509f5bf7a3a8d01dc434c41803
smartlove730 <55316874+smartlove730@users.noreply.github.com>
2026-02-10 17:54:33 +0530

    Redesign footer with logo, policy, top categories and blogs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Blog;
+use App\Models\Category;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +24,31 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+
+        View::composer('layouts.app', function ($view) {
+            $country = session('country') ?? 183;
+
+            $topFooterCategories = Category::query()
+                ->where('status', 1)
+                ->where('country_id', $country)
+                ->withCount(['blogs' => fn($query) => $query->where('status', 1)])
+                ->orderByDesc('blogs_count')
+                ->orderBy('name')
+                ->take(5)
+                ->get();
+
+            $topFooterBlogs = Blog::query()
+                ->where('status', 1)
+                ->where('country_id', $country)
+                ->latest()
+                ->take(5)
+                ->get();
+
+            $view->with([
+                'topFooterCategories' => $topFooterCategories,
+                'topFooterBlogs' => $topFooterBlogs,
+            ]);
+        });
     }
         
 }
