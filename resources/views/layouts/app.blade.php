b7505c0565eb71274020542f4dbb618a96ba534e
smartlove730 <55316874+smartlove730@users.noreply.github.com>
2026-02-10 14:06:26 +0530

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,6 +29,17 @@
 }
 
 </style>
+
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-N4CGBWESRQ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-N4CGBWESRQ');
+</script>
+    
 </head>
 <body>
 
